5a1b79e02d0b35243423fab1afd3d5acbc45ba63
Nikhil Mirpuri <ndmirpuri@gmail.com>
2026-01-15 21:28:06 -0600

    Create cart.js

---
+++ pages/cart.js
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from 'react'
+
+const CartContext = createContext()
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([])
+
+  const addToCart = product => setCart([...cart, product])
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export const useCart = () => useContext(CartContext)
